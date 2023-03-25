de097ff5f0200f666a0d4168039b362272fda350
mah-andrade <mahandradesantos.01@gmail.com>
2023-03-24 21:47:22 -0300

    Exceção para objeto nao encontrado

---
+++ src/main/java/com/matheus/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.matheus.helpdesk.resources.exceptions;
+
+import com.matheus.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex,
+                                                                 HttpServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+                "Object Not Found",ex.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+}
--- src/main/java/com/matheus/helpdesk/services/TecnicoService.java
+++ src/main/java/com/matheus/helpdesk/services/TecnicoService.java
@@ -2,6 +2,7 @@ package com.matheus.helpdesk.services;
 
 import com.matheus.helpdesk.domain.Tecnico;
 import com.matheus.helpdesk.repositories.TecnicoRepository;
+import com.matheus.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = tecnicoRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+ id));
     }
 }
