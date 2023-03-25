b2d7f146a03df24c72c8ff2379384a9fe7cb4885
mah-andrade <mahandradesantos.01@gmail.com>
2023-03-25 10:46:32 -0300

    Exeção para Cpf e Email ja cadastrado

--- src/main/java/com/matheus/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/matheus/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.matheus.helpdesk.resources.exceptions;
 
+import com.matheus.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.matheus.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +12,21 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex,
                                                                  HttpServletRequest request){
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
                 "Object Not Found",ex.getMessage(),request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+                                                                 HttpServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Violação de dados",ex.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
 }
--- src/main/java/com/matheus/helpdesk/services/TecnicoService.java
+++ src/main/java/com/matheus/helpdesk/services/TecnicoService.java
@@ -1,8 +1,11 @@
 package com.matheus.helpdesk.services;
 
+import com.matheus.helpdesk.domain.Pessoa;
 import com.matheus.helpdesk.domain.Tecnico;
 import com.matheus.helpdesk.dtos.TecnicoDTO;
+import com.matheus.helpdesk.repositories.PessoaRepository;
 import com.matheus.helpdesk.repositories.TecnicoRepository;
+import com.matheus.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.matheus.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,8 @@ public class TecnicoService {
     @Autowired
     TecnicoRepository tecnicoRepository;
 
+    @Autowired
+    PessoaRepository pessoaRepository;
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = tecnicoRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+ id));
@@ -27,7 +32,20 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO objDto) {
         objDto.setId(null);
+        validaPorCpfEmail(objDto);
         Tecnico newObj = new Tecnico(objDto);
         return tecnicoRepository.save(newObj);
+    }
+
+    private void validaPorCpfEmail(TecnicoDTO objDto) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDto.getCpf());
+        if(obj.isPresent() && obj.get().getId() != objDto.getId()){
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+        }
+        obj = pessoaRepository.findByEmail(objDto.getEmail());
+        if(obj.isPresent()&& obj.get().getId() != objDto.getId()){
+            throw new DataIntegrityViolationException("Email já cadastrado no sistema!");
+        }
+
     }
 }
